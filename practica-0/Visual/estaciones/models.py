7d80480978dc4f98b358a9e6c4d120e90553edfc
Andrys05 <saldanamarte@gmail.com>
2024-08-15 17:13:56 -0400

    Modelo para guardar la inf recibida

--- practica-0/Visual/estaciones/models.py
+++ practica-0/Visual/estaciones/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class DatoEstacion(models.Model):
+    id_estacion = models.IntegerField()
+    fecha = models.DateTimeField()
+    temperatura = models.FloatField()
+    humedad = models.FloatField()
+    presion = models.FloatField()
+    velocidad_viento = models.FloatField()
+    direccion_viento = models.FloatField()
+    pluvialidad = models.FloatField()
+
+    def __str__(self):
+        return f"Estación {self.id_estacion} - Fecha: {self.fecha}"
