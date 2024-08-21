7ac9a3f5a6ceb5d4296d82895d5d7cb7cb655f11
Andrys05 <saldanamarte@gmail.com>
2024-08-21 14:53:15 -0400

    ruta de registro a login

--- Final/estacion_P/estacion_A/urls.py
+++ Final/estacion_P/estacion_A/urls.py
@@ -3,5 +3,6 @@ from . import views  # Importar las vistas desde el archivo views.py
 
 urlpatterns = [
    path('', views.login_view, name='login'),  # Página principal redirige a login
+   path('login/', views.login_view, name='login'),  # Nueva ruta para /login/
    path('register/', views.register_view, name='register'),
 ]
