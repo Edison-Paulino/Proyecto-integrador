d937ee23ef77e77adcf38c0e8baef4b140b7f54c
Andrys05 <saldanamarte@gmail.com>
2024-08-26 16:43:29 -0400

    quitar ws completo

--- Final/estacion_P/estacion_A/routing.py
+++ Final/estacion_P/estacion_A/routing.py
@@ -1,7 +1,2 @@
 from django.urls import path
 from .consumers import EstacionConsumer, AlertaEstacionConsumer
-
-websocket_urlpatterns = [
-    path('ws/estaciones/', EstacionConsumer.as_asgi()),
-    path('ws/alertas/', AlertaEstacionConsumer.as_asgi()),
-]
